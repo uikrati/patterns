61f888f7bfbde6f59c47dcd3207642d3b3dbf015
uikrati <krati.sharma.one@gmail.com>
2022-10-16 13:03:43 +0530

    solidRhombus

--- solidRhombus.java
+++ solidRhombus.java
@@ -5,13 +5,13 @@ public class solidRhombus {
         int n = 5;
         for (int i = 0; i <n; i++) {
             for (int j = n-i; j > 0; j--) {
-                System.out.print(".");
+                System.out.print(" ");
             }
             for (int k = 0; k < n; k++) {
                 System.out.print("*");
             }
             for (int j = 0; j < i; j++) {
-                System.out.print(".");
+                System.out.print(" ");
             }
             System.out.println();
 
